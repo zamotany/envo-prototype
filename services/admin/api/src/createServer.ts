3da46309200c572604469356b05937cad7b02753
zamotany <pawel.trysla@callstack.com>
2022-06-11 20:25:33 +0200

    feat(admin/api): improve caching in Dockerfile

--- services/admin/api/src/createServer.ts
+++ services/admin/api/src/createServer.ts
@@ -3,5 +3,7 @@ import fastify, { FastifyServerOptions } from 'fastify';
 export async function createServer(options?: FastifyServerOptions) {
   const app = fastify(options);
 
+  app.get('/', () => ({ status: 'ok' }));
+
   return app;
 }
--- services/admin/api/src/main.ts
+++ services/admin/api/src/main.ts
@@ -4,7 +4,7 @@ async function main() {
   try {
     // TODO: configure pino to emit std{out,err}.log
     const server = await createServer({ logger: true });
-    await server.listen({ port: 3000 });
+    await server.listen({ port: 3000, host: '0.0.0.0' });
   } catch (error) {
     console.error(error);
     process.exit(1);
